07bf658257350a836f16d8f3f20a2cad9242a591
danzinazevedo <148352990+danzinazevedo@users.noreply.github.com>
2024-08-22 11:39:27 -0300

    commit inicial

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('form-sorteador').addEventListener('submit', function(evento){
-        evento.preventDefault();
+    document.getElementById('form-sorteador').addEventListener('submit', function(event){
+        event.preventDefault();
+        
         let numeroMaximo = document.getElementById('numero-maximo').value;
         numeroMaximo = parseInt(numeroMaximo);
 
         let numeroAleatorio = Math.random() * numeroMaximo;
-        numeroAleatorio = Math.floor(numeroAleatorio + 1);
+        numeroAleatorio=Math.floor(numeroAleatorio + 1)
 
-        document.getElementById('resultado-valor').innerText = numeroAleatorio;
-        document.querySelector(".resultado").style.display = 'block';
+        document.getElementById('resultado-valor').innerText =numeroAleatorio;
+        document.querySelector('.resultado').style.display = 'block';
     })
 })
